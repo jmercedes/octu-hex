05da2b969922b125084fe45f4e9edd731464c28e
Juan Mercedes <j.mercedes@thedesignpill.com>
2014-11-08 12:52:52 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MinistryOfSports::Application.config.secret_token = 'f31a0246e1805ab9f496e1c03d7e8767900e03fa849b6df3e608ab1b6deb57d13eb9bf96687b8d04c153b713316182a9e872467a7ce7c34deb7e07994aaeee1f'
